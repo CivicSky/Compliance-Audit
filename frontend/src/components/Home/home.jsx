765deac6c20a77715e1d177296314748849de1f9
Ru1ean <javellanavja@gmail.com>
2025-10-07 02:03:12 +0800

    Moved frontend branch files into frontend folder

---
+++ frontend/src/components/Home/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = ()=>{
+    return(
+        <div className="mt-20 text-center">
+           
+        </div>
+    );
+};
+
+
+
+export default Home;
