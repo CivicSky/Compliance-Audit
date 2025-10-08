605c1b1ac0fbbc8a494cb06b23cbdd2d3a13ea28
Ru1ean <javellanavja@gmail.com>
2025-10-08 09:46:46 +0800

    Restore backend folder

--- backend/routes/offices.js
+++ backend/routes/offices.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
 
-router.get('/', userController.getUserHome);
-router.get('/profile', userController.getUserProfile);
-router.get('/settings', userController.getUserSettings);
-router.post('/add', userController.addUser);
-router.get('/view', userController.listUsers);
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+// Basic route for testing
+router.get('/', (req, res) => {
+  res.send('Offices route working');
+});
 
 module.exports = router;
